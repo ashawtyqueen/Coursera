1ea0eaa17e5060ddb4bfc92f9397874a991de4c6
Tokede Anuoluwapo Ifeoluwa <82208634+ashawtyqueen@users.noreply.github.com>
2023-03-02 13:16:18 +0100

    Add files via upload

---
+++ revision1.js
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrapbundle.min.js"></script>;
+
+var name = "ifeoluwa";
+var age = "26";
+final = "name" + "age";
+console.log(final);
